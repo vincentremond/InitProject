6c244dbbe39017156fa3130dce87cc392eeb0f96
Vincent Rémond <vincent.remond-ext@kerialis.fr>
2024-04-18 16:43:44 +0200

    Refactor test project initialization and build process

--- Install-as-dotnet-tool.ps1
+++ Install-as-dotnet-tool.ps1
@@ -1,4 +1,6 @@
 $ToolName = "InitProject"
+dotnet tool restore
+dotnet build
 dotnet pack --configuration Release
 dotnet tool uninstall $ToolName --global
 dotnet tool install $ToolName --add-source .\$ToolName\nupkg\ --global
